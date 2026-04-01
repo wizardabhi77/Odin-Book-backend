2c9497a95918458e10cb89f014252b0894293fb2
wizardabhi77 <vemulapalliabhinav@gmail.com>
2026-04-01 20:06:16 +0530

    cloudinary fix

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: " dkgyxxpkd",
+  cloud_name: "dkgyxxpkd",
   api_key: "681325432692695",
   api_secret: "hy3mMmerKYDKUZVwguYRvI5SGh0"
 });
